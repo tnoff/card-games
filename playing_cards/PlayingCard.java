3f12223690942427a0eadf5405ea6275c927db7c
tnoff <tylernorth18@gmail.com>
2017-04-18 01:17:29 -0700

    add super bare bones cards classes
    deck still needs some extra work

---
+++ playing_cards/PlayingCard.java
@@ -0,0 +1,34 @@
+public class PlayingCard {
+    private int suite;
+    private int rank;
+
+    private String[] card_ranks = {"2", "3", "4", "5", "6", "7", "8", "9",
+                                   "10", "J", "Q", "K", "A"};
+
+    private String[] card_suites = {"Spades", "Clubs", "Diamonds", "Hearts"};
+
+    public PlayingCard(int input_rank, int input_suite) throws PlayingCardException {
+        // Rank of card, 0 - 12 integer
+        // Suite of card, 0 - 3 integer
+        if ((input_rank < 0 ) || (input_rank > 12)){
+            throw new PlayingCardException();
+        }
+        if ((input_suite < 0) || (input_suite > 3)){
+            throw new PlayingCardException();
+        }
+        rank = input_rank;
+        suite = input_suite;
+    }
+
+    public int getRank(){
+        return rank;
+    }
+
+    public int getSuite(){
+        return suite;
+    }
+
+    public String toString(){
+        return "" + card_ranks[rank] + "-" + card_suites[suite];
+    }
+}
---
+++ playing_cards/PlayingCardDeck.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class PlayingCardDeck {
+    ArrayList<PlayingCard> card_deck;
+
+    public PlayingCardDeck(){
+        // Init basic card deck
+        card_deck = new ArrayList<PlayingCard>();
+
+        // temp variable
+        PlayingCard new_card;
+
+        for (int rank = 0; rank < 13; rank++){
+            for (int suite = 0; suite < 4; suite++){
+                try {
+                    new_card = new PlayingCard(rank, suite);
+                    card_deck.add(new_card);
+                    // safe variables so pretty much ignore catch
+                } catch (PlayingCardException e){}
+            }
+        }
+    }
+
+    public void shuffle(){
+        Collections.shuffle(card_deck);
+    }
+
+    public void addPlayingCard(PlayingCard new_card){
+        card_deck.add(new_card);
+    }
+
+    public PlayingCard getPlayingCard(int index){
+        // TODO check index
+        return card_deck.get(index);
+    }
+
+    public PlayingCard removePlayingCard(int index){
+        // TODO check index
+        return card_deck.remove(index);
+    }
+
+    public int getDeckSize(){
+        return card_deck.size();
+    }
+
+    public String toString(){
+        String total_string = "";
+        for (int x = 0; x < card_deck.size(); x++){
+            total_string = total_string + card_deck.get(x);
+            if ( x != card_deck.size() - 1){
+                total_string = total_string + "--";
+            }
+        }
+        return total_string;
+    }
+}
