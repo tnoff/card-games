29e0cb29155e2281b0a818383be425c2633f5cd8
tnoff <tylernorth18@gmail.com>
2017-04-22 04:34:20 -0700

    add hand to player class

--- people/Player.java
+++ people/Player.java
@@ -1,9 +1,39 @@
 package people;
 
+import playing_cards.PlayingCard;
+import playing_cards.PlayingCardException;
+import playing_cards.PlayingCardHand;
+
 public class Player extends Person {
+    // Player Hand
+    private PlayingCardHand hand;
 
     public Player(String input_name, int input_money){
         super(input_name, input_money);
+        hand = new PlayingCardHand();
     }
 
+    public PlayingCard addCard(PlayingCard new_card){
+        return hand.addCard(new_card);
+    }
+
+    public PlayingCard removeCard(int index) throws PersonException {
+        try {
+            return hand.removeCard(index);
+        } catch ( PlayingCardException e ){
+            throw new PersonException();
+        }
+    }
+
+    public PlayingCard getCard(int index) throws PersonException {
+        try {
+            return hand.getCard(index);
+        } catch ( PlayingCardException e ){
+            throw new PersonException();
+        }
+    }
+
+    public int handSize(){
+        return hand.getSize();
+    }
 }
